80f67b8414707921f4569a80add42162ab8da508
polinas888 <p.shcherbinina@itransition.com>
2023-02-22 14:18:01 +0300

    set up API_KEY and BASE_URL for Retrofit

---
+++ app/src/main/java/com/example/weathercomposeapp/network/ApiRetrofitFactory.kt
@@ -0,0 +1,34 @@
+package com.example.weathercomposeapp.network
+
+import com.example.weathercomposeapp.BuildConfig
+import com.example.weathercomposeapp.utils.Constants
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiRetrofitFactory {
+    private val authInterceptor = Interceptor { chain ->
+        val newUrl = chain.request().url
+            .newBuilder()
+            .addQueryParameter("key", BuildConfig.API_KEY)
+            .build()
+
+        val newRequest = chain.request()
+            .newBuilder()
+            .url(newUrl)
+            .build()
+
+        chain.proceed(newRequest)
+    }
+
+    private val httpClient = OkHttpClient().newBuilder()
+        .addInterceptor(authInterceptor)
+        .build()
+
+    fun weatherApiRetrofit(): Retrofit = Retrofit.Builder()
+        .client(httpClient)
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
